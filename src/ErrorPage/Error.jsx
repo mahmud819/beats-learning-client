67d9215c5a746815e3bffeac827fe0d815f98bd3
mahmud819 <mahmudurr819@gmail.com>
2025-11-29 22:23:48 +0600

    form validation completed

--- src/ErrorPage/Error.jsx
+++ src/ErrorPage/Error.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const Error = () => {
     const error = useRouteError();
   console.error(error);
   return (
-    <div>
+    <div id="error-page" className="flex flex-col justify-center items-center lg:pt-[20%]">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
         <i>{error.statusText || error.message}</i>
       </p>
+      <Link className="btn mt-4" to='/' >Go Home</Link>
     </div>
   );
 };
--- src/Regester/Regester.jsx
+++ src/Regester/Regester.jsx
@@ -1,24 +1,71 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { FaUserAlt } from "react-icons/fa";
 import { FaFacebook, FaGoogle, FaRegUser, FaUser } from "react-icons/fa6";
 import { MdOutlineMailOutline } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../AuthProvider/AuthProvider";
-
+import Swal from "sweetalert2";
+import { validatePassword } from "firebase/auth";
 
 const Regester = () => {
-  const {creatUser} = useContext(AuthContext)
+  const { createUser } = useContext(AuthContext);
+  // use states
 
+  
+  const [err,setErr] = useState('');
+  const [showpassword,setShowPassword]=useState(false);
 
-  const handleSignUp=(e)=>{
+  // show password function
+
+  const handleShowPassword=()=>{
+    setShowPassword(!true);
+  }
+
+  // sign up function
+
+  const handleSignUp = (e) => {
     e.preventDefault();
-    const form=e.target;
+    const form = e.target;
     const name = form.name.value;
     const email = form.email.value;
     const password = form.password.value;
-    console.log(name,email,password);
-  }
+    console.log(name, email, password);
+
+    setErr('');
+    // password length check function
+
+    if(password.length<8){
+        setErr('Password should be 8 character or longer');
+        return;
+      }
+
+    // input password validation
+
+      const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+\-={}[\]|:;"'<>,.?/~`]).{8,}$/;
+      
+      if(!passwordRegex.test(password)){
+        setErr('Password should be one upper case, one lower case, one number,one special character');
+        return;
+      }
+
+    
+    // user create function
+
+    createUser(email, password)
+      .then((userCredential) => {
+        Swal.fire({
+          title: "Account create successful",
+          icon: "success",
+          draggable: true,
+        });
+      })
+      .catch((error) => {
+        console.log(error, error.message);
+        setErr(error.message);
+      });
+      form.reset();
+  };
   return (
     <div className=" flex min-h-screen bg-base-300 justify-center items-center ">
       {/* login main div */}
@@ -32,16 +79,15 @@ const Regester = () => {
             {/* User name section */}
 
             <div className="flex ">
-
               <div className="w-1/2">
                 <label htmlFor="">User Name</label>
                 <div className="flex justify-center items-center p-2 ">
-                  <FaRegUser className="mt-2 -ml-6" />
+                  <FaRegUser className="mt-1 -ml-6" />
                   <input
                     className="mt-1 ml-2 border-b-2 outline-none"
                     placeholder="User Name"
                     type="text"
-                    name='name'
+                    name="name"
                     required
                   />
                 </div>
@@ -52,7 +98,7 @@ const Regester = () => {
               <div className="1/2">
                 <label htmlFor="">Email</label>
                 <div className="flex justify-center items-center p-2 ">
-                  <MdOutlineMailOutline className="mt-2 -ml-2" />
+                  <MdOutlineMailOutline className="mt-1 -ml-2" />
                   <input
                     className="mt-1 ml-2 border-b-2 outline-none"
                     placeholder="Email"
@@ -60,6 +106,7 @@ const Regester = () => {
                     name="email"
                     required
                   />
+                  
                 </div>
               </div>
             </div>
@@ -73,23 +120,25 @@ const Regester = () => {
               <div className="flex  p-2 ">
                 <RiLockPasswordLine className="mt-2 -ml-2" />
                 <input
-                  className="mt-1 border-b-2 outline-none"
+                  className="w-[90%] mt-1 border-b-2 outline-none pl-2"
                   placeholder="Password"
                   type="Password"
                   name="password"
                   required
                 />
+                <i class="fa-regular fa-eye text-black -ml-6"></i>
               </div>
             </div>
 
+            {/* validation error messages  */}
+            
+            {err && <p>{err}</p>}
             <button className="mt-1 bg-[#54B8FF] p-2">Sign Up</button>
           </form>
           <div className="text-center">
             <Link to="/login" className="text-center text-sm mx-auto">
               Already have an account?{" "}
-              <Link to="/login" className="text-[#54B8FF]">
-                Sign In
-              </Link>
+              <span className="text-[#54B8FF]">Sign In</span>
             </Link>
           </div>
 
